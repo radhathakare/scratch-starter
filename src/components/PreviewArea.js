6f8e348bfc74d09b1d55666e58a526e09df8d6b8
radhathakare <radhathakare322@gmail.com>
2024-08-05 15:42:41 +0530

    add

--- src/components/PreviewArea.js
+++ src/components/PreviewArea.js
@@ -1,34 +1,38 @@
-import React, { useState } from "react";
+import React from "react";
 import CatSprite from "./CatSprite";
 import { connect } from "react-redux";
 import { addCharacter, setActive } from "../redux/character/actions";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
 
-
 // Styling for MaterialUI Components
 const useStyles = makeStyles((theme) =>
   createStyles({
-    formControl: {
-      margin: theme.spacing(1),
-      minWidth: 120,
+    characterContainer: {
+      position: 'absolute',
     },
-    selectEmpty: {
-      marginTop: theme.spacing(2),
+    messageBox: {
+      display: 'none',
+      border: '2px solid',
+      padding: theme.spacing(2),
+      marginLeft: theme.spacing(3),
+      marginBottom: theme.spacing(2),
     },
-    button: {
-      margin: 0,
-    },
+    messageBox1: {
+      display: 'none',
+      border: '2px solid',
+      borderRadius: '50%',
+      width: '16px',
+      height: '16px',
+      position: 'absolute',
+      left: '50%',
+      marginLeft: theme.spacing(3),
+      marginBottom: theme.spacing(2),
+    }
   })
 );
 
 function PreviewArea({ character, set_active }) {
   const classes = useStyles();
-  const [active, setActive] = useState(character.active);
-  var pos1 = 0,
-    pos2 = 0,
-    pos3 = 0,
-    pos4 = 0;
-
   let elmnt = null;
 
   function dragMouseDown(e, id) {
@@ -36,91 +40,68 @@ function PreviewArea({ character, set_active }) {
 
     e = e || window.event;
     e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
+    let pos3 = e.clientX, pos4 = e.clientY;
+
+    function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      const pos1 = pos3 - e.clientX;
+      const pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+
+      elmnt.style.top = `${elmnt.offsetTop - pos2}px`;
+      elmnt.style.left = `${elmnt.offsetLeft - pos1}px`;
+    }
+
+    function closeDragElement() {
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+
     document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
   }
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-
-    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-
-  // handle changing active character
-  const handleChange = (e) => {
-    setActive(e.target.value);
-    set_active(e.target.value);
-  };
-
   return (
-    <div
-      className="w-full flex-none h-full overflow-y-auto p-3"
-      id="preview_area"
-    >
+    <div className="w-full flex-none h-full overflow-y-auto p-3" id="preview_area">
       <div className="flex justify-between mb-10">
-       
-
-        
+        {/* Add any additional UI elements here */}
       </div>
       <div className="flex justify-around h-full">
-        {character.characters.map((x, i) => {
-          return (
-            <div
-              id={`${x.id}-${i}`}
-              key={i}
-              className={`absolute`}
-              onMouseDown={(e) => dragMouseDown(e, `${x.id}-${i}`)}
-            >
-              <div id={`${x.id}-div`} className="character">
-                <div
-                  className="hidden border-2 p-2 ml-3 mb-2 w-auto whitespace-nowrap"
-                  id={x.id + "-message-box"}
-                ></div>
-                <div
-                  className="hidden rounded-full border-2 w-4 left-1/2 h-4 ml-3 mb-2 whitespace-nowrap"
-                  id={x.id + "-message-box1"}
-                ></div>
-                <CatSprite charac_id={x.id} />
-              </div>
+        {character.characters.map((x, i) => (
+          <div
+            id={`${x.id}-${i}`}
+            key={i}
+            className={classes.characterContainer}
+            onMouseDown={(e) => dragMouseDown(e, `${x.id}-${i}`)}
+          >
+            <div id={`${x.id}-div`} className="character">
+              <div
+                className={classes.messageBox}
+                id={`${x.id}-message-box`}
+              ></div>
+              <div
+                className={classes.messageBox1}
+                id={`${x.id}-message-box1`}
+              ></div>
+              <CatSprite charac_id={x.id} />
             </div>
-          );
-        })}
+          </div>
+        ))}
       </div>
     </div>
   );
 }
 
-// mapping state to props
-const mapStateToProps = (state) => {
-  return {
-    character: state.character,
-  };
-};
+// Mapping state and dispatch to props
+const mapStateToProps = (state) => ({
+  character: state.character,
+});
 
-// mapping functions to components
-const mapDispatchToProps = (dispatch) => {
-  return {
-    add_character: () => dispatch(addCharacter()),
-    set_active: (ch_id) => dispatch(setActive(ch_id)),
-  };
-};
+const mapDispatchToProps = (dispatch) => ({
+  add_character: () => dispatch(addCharacter()),
+  set_active: (ch_id) => dispatch(setActive(ch_id)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(PreviewArea);
